289f6162ab0931fecbf84a7f6c9abad8c7b70b17
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-10-25 18:44:56 +0900

    todo컴포넌트 생성 및 ui컴포넌트 완성

---
+++ src/components/TodoList.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+import TodoItem from "../TodoItem";
+function TodoList() {
+  return (
+    <TodoListBlock>
+      <TodoItem text="프로젝트 생성하기" done={true} />
+      <TodoItem text="컴포넌트 스타일링 하기" done={true} />
+      <TodoItem text="Context 만들기" done={false} />
+      <TodoItem text="기능 구현하기" done={false} />
+    </TodoListBlock>
+  );
+}
+
+const TodoListBlock = styled.div`
+  flex: 1;
+  padding: 20px 32px 48px 32px;
+  overflow-y: auto;
+`;
+
+export default TodoList;
