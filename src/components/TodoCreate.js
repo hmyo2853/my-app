f34b5ef707b33d697a5e7216aec0d8d0d88fe43e
hmyo2853 <35989487+hmyo2853@users.noreply.github.com>
2022-11-02 20:38:01 +0900

    벨로퍼트 투두리스트 클론 코딩 완

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,16 +1,41 @@
 import styled, { css } from "styled-components";
 import { MdAdd } from "react-icons/md";
 import { useState } from "react";
+import { useTodoDispatch, useTodoNextId } from "../TodoContext";
 
 function TodoCreate() {
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
+
   const onToggle = () => setOpen(!open);
+  const onChange = (e) => setValue(e.target.value);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch({
+      type: "CREATE",
+      todo: {
+        id: nextId.current,
+        text: value,
+        done: false,
+      },
+    });
+    setValue("");
+    setOpen(false);
+    nextId.current += 1;
+  };
   return (
     <div>
       {open && (
         <InsertFormPositioner>
-          <InsertForm>
-            <Input autoFocus placeholder="할 일을 입력후 Enter" />
+          <InsertForm onSubmit={onSubmit}>
+            <Input
+              autoFocus
+              placeholder="할 일을 입력후 Enter"
+              onChange={onChange}
+              value={value}
+            />
           </InsertForm>
         </InsertFormPositioner>
       )}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,19 @@
 import styled from "styled-components";
+import { useTodoState } from "../TodoContext";
 import TodoItem from "../TodoItem";
 function TodoList() {
+  const todos = useTodoState();
+
   return (
     <TodoListBlock>
-      <TodoItem text="프로젝트 생성하기" done={true} />
-      <TodoItem text="컴포넌트 스타일링 하기" done={true} />
-      <TodoItem text="Context 만들기" done={false} />
-      <TodoItem text="기능 구현하기" done={false} />
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          text={todo.text}
+          done={todo.done}
+        />
+      ))}
     </TodoListBlock>
   );
 }
